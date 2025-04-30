1a247cb8652ca1f047b551495658240a97604f58
CKrawczyk <coleman.krawczyk@gmail.com>
2025-04-30 14:56:43 +0100

    Make __init__ files do useful things
    Import all the profiles into a useful structure.  Also register all the new profiles with Herculens on import.

---
+++ jax_lensing_profiles/MassModel/Profiles/__init__.py
@@ -0,0 +1,6 @@
+from .gaussian_ellipse_kappa_jax import GaussianEllipseKappa
+from .gaussian_kappa_jax import GaussianKappa
+from .MGE_jax import MGE
+from .multi_gaussian_ellipse_kappa_jax import MultiGaussianEllipseKappa
+from .NFW import NFW
+from .NFW import TNFW
